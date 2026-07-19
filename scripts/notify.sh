6e9b75b795ed67b6cab435dbbc31361c1dc0f425
Adithya-0606 <adithya3006.a@example.com>
2026-07-19 09:51:16 +0000

    Test webhook

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -3,7 +3,7 @@
 STATUS=$1
 
 if [ "$STATUS" = "success" ]; then
-    MESSAGE="✅ Jenkins Build Successful! Project: $JOB_NAME Build: #$BUILD_NUMBER"
+    MESSAGE="✅ Jenkins Build Successful!!!!!!!!!!! Project: $JOB_NAME Build: #$BUILD_NUMBER"
 else
     MESSAGE="❌ Jenkins Build Failed! Project: $JOB_NAME Build: #$BUILD_NUMBER"
 fi
